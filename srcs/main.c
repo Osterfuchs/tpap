0f01799d95341ba4fa2a083bf0d9a5b05fcd9a75
Osterfuchs <thefoxylia@gmail.com>
2025-10-10 00:18:35 +0200

    tokenize

--- srcs/main.c
+++ srcs/main.c
@@ -3,30 +3,44 @@
 
 #define BUFFER_LEN 128
 
-int main() {
-	FILE *file = fopen("HelloWorld.tpap", "r");
+int main(int argc, char **argv) {
+	if(argc < 2) {
+		puts("expected filename");
+		return EXIT_FAILURE;
+	}
+	FILE *file = fopen(argv[1], "r");
+	if(!file) {
+		puts("Unable to open file");
+		return EXIT_FAILURE;
+	}
+
 	size_t len;
 	char *buffer = (char*) malloc(BUFFER_LEN), *const more = (char*) malloc(BUFFER_LEN);
-
-	if(!file) {
-		puts("no");
-		return 1;
-	}
 
 	while(getline(&buffer, &len, file) != -1) {
 		int j = 0;
 		for(int i = 0; i < BUFFER_LEN && buffer[i] != '\n' && buffer[i] != '\0' && buffer[i] != EOF; i++) {
-			if( buffer[i] == ' ' ) {
-				more[j] = '\0';
-				puts(more);
-				j=0;
-			} else {
+			if( (buffer[i] >= 'a' && buffer[i] <= 'z') || (buffer[i] >= 'A' && buffer[i] <= 'Z') || buffer[i] == '_' ) {
 				more[j] = buffer[i];
 				j++;
+			} else if( buffer[i] == ' ' || buffer[i] == '\n' || buffer[i] == '\t' ) {
+				more[j] = '\0';
+				if(j > 0) {
+					puts(more);
+				}
+				j=0;
+			} else {
+				more[j] = '\0';
+				if(j > 0) {
+					puts(more);
+				}
+				j=0;
+				putchar(buffer[i]);
+				putchar('\n');
 			}
 		}
-		more[j] = '\0';
-		puts(more);
+//		more[j] = '\0';
+//		puts(more);
 	}
 
 	return 0;
